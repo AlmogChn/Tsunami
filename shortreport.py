9214c11e5940c01dfede9ff5038abdc081672405
AlmogChn <108393881+AlmogChn@users.noreply.github.com>
2022-11-28 21:54:11 +0200

    Update shortreport.py

--- shortreport.py
+++ shortreport.py
@@ -1,18 +1,22 @@
 import json
+
 with open("/tmp/tsunami-output.json", 'r', encoding='utf-8') as data_file:
-  data = json.load(data_file)
+    data = json.load(data_file)
 
-status = data['scanStatus']
-ip = data['scanFindings'][0]['networkService']['networkEndpoint']['ipAddress']['address']
-port = data['scanFindings'][0]['networkService']['networkEndpoint']['port']['portNumber']
-software = data['scanFindings'][0]['networkService']['serviceContext']['webServiceContext']['software']['name']
-issue = data['scanFindings'][0]['vulnerability']['title']
-severity = data['scanFindings'][0]['vulnerability']['severity']
-info = data['scanFindings'][0]['vulnerability']['description']
-recom =data['scanFindings'][0]['vulnerability']['recommendation']
-dtime = data['scanStartTimestamp']
+try:
+    status = data['scanStatus']
+    ip = data['scanFindings'][0]['networkService']['networkEndpoint']['ipAddress']['address']
+    port = data['scanFindings'][0]['networkService']['networkEndpoint']['port']['portNumber']
+    software = data['scanFindings'][0]['networkService']['serviceContext']['webServiceContext']['software']['name']
+    issue = data['scanFindings'][0]['vulnerability']['title']
+    severity = data['scanFindings'][0]['vulnerability']['severity']
+    info = data['scanFindings'][0]['vulnerability']['description']
+    recom = data['scanFindings'][0]['vulnerability']['recommendation']
+    dtime = data['scanStartTimestamp']
 
-print('Scan Status {}, \nIp Address : {} \nPort Number : {}\n'
-      'Software : {}\nVulnerability : {} \nSeverity : {}\n'
-      'Description : {}\nRecommendation : {} \nScan run time : {}'
-      '  '.format(status, ip, port, software, issue, severity, info, recom,dtime ))
+    print('Scan Status {}, \nIp Address : {} \nPort Number : {}\n'
+          'Software : {}\nVulnerability : {} \nSeverity : {}\n'
+          'Description : {}\nRecommendation : {} \nScan run time : {}'
+          '  '.format(status, ip, port, software, issue, severity, info, recom, dtime))
+except ValueError as v_error:
+    print(v_error)
